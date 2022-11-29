00fd329a46f95835ff3ed719b41d2e5c756d9cb7
bmay <bloodymay456@gmail.com>
2022-11-29 22:03:35 +0300

    seminar1

---
+++ seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+    int inputNumber= int.Parse(inputLine);
+
+    Console.WriteLine(Math.Pow(int.Parse(inputLine),2));
+
+}
---
+++ seminar1_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLine = Console.ReadLine();
+if (inputLine!=null)
+{
+
+    string[] dayOfWeek = new string[7];
+    dayOfWeek[0] = "Понедельник"; 
+    dayOfWeek[1] = "Вторник"; 
+    dayOfWeek[2] = "Среда"; 
+    dayOfWeek[3] = "Четверг"; 
+    dayOfWeek[4] = "Пятница"; 
+    dayOfWeek[5] = "Суббота"; 
+    dayOfWeek[6] = "Воскресенье"; 
+
+    int inputNum = int.Parse(inputLine);
+
+    string outDayOfWeek = dayOfWeek[inputNum - 1];
+    
+    Console.WriteLine(outDayOfWeek);
+
+}
