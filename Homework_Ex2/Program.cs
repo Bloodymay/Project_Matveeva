a4b2c1112450f8d3154421a04e9bab33e818831c
bmay <bloodymay456@gmail.com>
2022-11-30 11:25:50 +0300

    first ex added

---
+++ Homework_Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿string? firstNumber = Console.ReadLine();
+string? secondNumber = Console.ReadLine();
+ if (firstNumber != null && secondNumber != null);
+
+ {
+    int num1 = int.Parse(firstNumber);
+    int num2 = int.Parse(secondNumber);
+    if (num1 > num2)
+    {
+        Console.WriteLine("num1 Большее число");
+        Console.WriteLine("num2 Меньшее число");
+
+    }
+    else 
+    {
+        Console.WriteLine("num1 Меньшее число");
+        Console.WriteLine("num2 Большее число");
+    }
+ }
