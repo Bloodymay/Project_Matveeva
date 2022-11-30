5c93a0e416ab9e6e66c5c0bf62e2ee209af0ce72
bmay <bloodymay456@gmail.com>
2022-11-30 14:08:10 +0300

    non-fixed code

---
+++ Homework_Ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿string? firstNumber = Console.ReadLine();
+string? secondNumber = Console.ReadLine();
+string? thirdNumber = Console.ReadLine();
+if (firstNumber != null && secondNumber != null && thirdNumber != null);
+{
+    int num1 = int.Parse(firstNumber);
+    int num2 = int.Parse(secondNumber);
+    int num3 = int.Parse(thirdNumber);
+   
+    
+    if (num1>num2)
+    
+    {
+        Console.WriteLine(num1);
+    }
+       
+    if (num2 > num3)
+
+    {
+        Console.WriteLine(num2);
+    }
+    else
+    {
+        Console.WriteLine(num3);
+    }
+} 
