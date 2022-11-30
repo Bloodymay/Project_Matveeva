1e10dfe6cf7bf04e08fa6198dabeeb3f51f77447
bmay <bloodymay456@gmail.com>
2022-11-30 15:55:42 +0300

    Ex6 added

---
+++ Homework_Ex6/Program.cs
@@ -0,0 +1,15 @@
+﻿string? insertNumber = Console.ReadLine();
+if (insertNumber != null);
+{
+    int num = int.Parse(insertNumber);
+    int a = num%2;
+    if (a==0)
+    {
+        Console.WriteLine("Четное");
+    } 
+
+    else
+    {
+        Console.WriteLine("Нечетное");
+    }
+}
