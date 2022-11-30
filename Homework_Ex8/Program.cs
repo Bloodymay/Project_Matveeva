f2b4ca3dd1a6887905b64639894c86ba2761a84d
bmay <bloodymay456@gmail.com>
2022-11-30 17:14:05 +0300

    Ex8 non-fixed

---
+++ Homework_Ex8/Program.cs
@@ -0,0 +1,17 @@
+﻿string? insertNumber = Console.ReadLine();
+if(insertNumber!= null);
+{
+int num = int.Parse(insertNumber);
+int a = 2;
+int outputNum = 0;
+while (a <= num);
+{
+
+    outputNum = outputNum + a;
+    a = a+2;
+
+}
+Console.WriteLine(outputNum);
+}   
+
+
