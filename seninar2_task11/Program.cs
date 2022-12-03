dc0461c13a82bb53399b8612730f8464c586077c
bmay <bloodymay456@gmail.com>
2022-12-03 15:41:32 +0300

    task11 added

---
+++ seninar2_task11/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Random numberSint = new System.Random();
+int number = numberSint.Next(100,1000);
+Console.WriteLine(number);
+int a  = number/100;
+int b = number%10;
+int result = a*10+b;
+Console.WriteLine(result);
