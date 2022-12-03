a4a7f1748261cd8a55fd05928326983055ab5648
bmay <bloodymay456@gmail.com>
2022-12-03 15:42:09 +0300

    task14 added

---
+++ seminar2_task14/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = int.Parse(Console.ReadLine()??"0");
+int res1 = num%7;
+int res2 = num%21;
+if ((res1==0)&&(res2==0))
+{
+    Console.WriteLine(num +" Делится на 7 и 21 без остатка.");
+}
+else
+{
+    Console.WriteLine("Число " + num + " Не кратно числам 7 и 21." );
+}
