9e769c57f4ba3bae59b3c9670134e5f494027e4b
bmay <bloodymay456@gmail.com>
2022-12-03 15:41:49 +0300

    task12 added

---
+++ seminar2_task12/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.ReadLine()??"0");
+int b = int.Parse(Console.ReadLine()??"0");
+int result = b%a;
+if (result==0)
+{
+    Console.WriteLine("Второе число делится на первое без остатка");
+  
+}
+else 
+{
+    Console.WriteLine("Второе число не кратно первому. Остаток от деления = "  + result);
+}
