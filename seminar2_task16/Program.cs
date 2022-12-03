4116b2a11564b7f265cf30fb2438025212284fb3
bmay <bloodymay456@gmail.com>
2022-12-03 15:42:21 +0300

    task16 added

---
+++ seminar2_task16/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+if ((num1 == num2*num2) || (num2 == num1*num1))
+{
+    Console.WriteLine(" Является квадратом числа ");
+}
+else
+{
+    Console.WriteLine("Не является квадратом числа " );
+}
+
+
