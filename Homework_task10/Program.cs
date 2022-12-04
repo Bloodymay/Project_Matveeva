b2c82c6dbbedcaf738fda39b3e9b91d732395e0f
bmay <bloodymay456@gmail.com>
2022-12-04 22:53:30 +0300

    task10 added

---
+++ Homework_task10/Program.cs
@@ -0,0 +1,11 @@
+﻿int insertNum = int.Parse(Console.ReadLine()??"0");
+
+if ((insertNum<1000)&&(insertNum>99))
+{
+ int result = (insertNum/10)%10;
+ Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine(" Вы ввели не трехзначное число. Введите другое. ");
+}
