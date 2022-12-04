b8484b02d8160ede5d868be288115736a6860c04
bmay <bloodymay456@gmail.com>
2022-12-04 22:53:50 +0300

    task13 added

---
+++ Homework_task13/Program.cs
@@ -0,0 +1,10 @@
+﻿int insertNum = int.Parse(Console.ReadLine()??"0");
+if (insertNum>99)
+{
+    char[] array = insertNum.ToString().ToCharArray();
+    Console.WriteLine(array[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
