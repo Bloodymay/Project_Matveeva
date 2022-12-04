8953ff1f372d16bf943bb3d0f1ac1f01d23e22f7
bmay <bloodymay456@gmail.com>
2022-12-04 22:54:10 +0300

    task15 added

---
+++ Homework_task15/Program.cs
@@ -0,0 +1,14 @@
+﻿int insertNum = int.Parse(Console.ReadLine()??"0");
+if ((insertNum>0)&&(insertNum<8))
+{
+    if (insertNum>5)
+    Console.WriteLine("Выходной");
+    else
+    {
+        Console.WriteLine("Рабочий");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число не является днем недели! Пожалуйста, введите число от 1 до 7.");
+}
