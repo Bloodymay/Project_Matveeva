159d77a331901b4abd1630b21f96a77eca4f4f97
bmay <bloodymay456@gmail.com>
2022-12-05 07:33:48 +0300

    task15_1 non-fixed added

---
+++ Homework_task15_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int insertNum = int.Parse(Console.ReadLine()??"0");
+string [] daysOfWeek = new string [6];
+daysOfWeek[0]=  "Рабочий"; daysOfWeek[1]="Рабочий"; daysOfWeek[2]= "Рабочий";
+daysOfWeek[3]=  "Рабочий"; daysOfWeek[4]= "Рабочий"; daysOfWeek[5]= "Выходной"; daysOfWeek[6] = "Выходной";
+if ((insertNum>0)&&(insertNum<8))
+{
+Console.WriteLine(daysOfWeek[insertNum - 1]);
+}
+else
+{
+    Console.WriteLine("Введенное число не соответствует дню  недели. Пожалуйста,выберите другое число от 1 до 7.");
+}
