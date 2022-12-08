4f6c1a0bc98c0a02f138113b06a3006277d29b61
bmay <bloodymay456@gmail.com>
2022-12-08 22:06:05 +0300

    task19 added

---
+++ seminar3_task19/Program.cs
@@ -0,0 +1,42 @@
+﻿int ReadData(string message)
+{
+    Console.WriteLine(message);
+   
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+bool Palindrome(int insertNum)
+{
+bool result = false;
+result = (insertNum/10000 == insertNum%10)&&((insertNum/1000)%10)==((insertNum/10)%10);
+return result;
+}
+void PrintData( string message, bool result)
+{
+    Console.WriteLine(message + result);
+}
+
+
+
+int insertNum = ReadData("Введите пятизначное число");
+if ((insertNum>9999)&&(insertNum<100000))
+
+{
+    if 
+    {
+        bool result = Palindrome(insertNum);
+        PrintData("Число является палиндромом", result);
+    }
+    
+    else
+    {
+         PrintData("Число не является палиндромом", result);
+    }     
+
+
+
+}
+else
+{
+    Console.WriteLine(insertNum + " - не является пятизначным числом. Введите другое");
+}
