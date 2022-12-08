c51968cfc0e611b33cf0494d8a776dc15e381695
bmay <bloodymay456@gmail.com>
2022-12-08 22:05:15 +0300

    task23 added

---
+++ Homework_task23/Program.cs
@@ -0,0 +1,36 @@
+﻿void PrintData( string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
+// Метод сборки строки
+
+string LineBuilder(int n, int pow)
+{
+    string line = string.Empty;
+    for (int i=1; i<n; i++)
+    {
+        line = line + Math.Pow (i, pow) + " , ";
+    }
+    line = line + Math.Pow (n, pow);
+    return line;
+}
+
+// Метод читает данные от пользователя
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+
+int insertNum = ReadData("Введите число: ");
+
+string outLine = LineBuilder(insertNum, 1 );
+PrintData(" ", outLine);
+
+outLine = LineBuilder(insertNum, 3 );
+PrintData(" ", outLine);
+
+
